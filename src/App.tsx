6006aece7c5a6873ccd14bdb5c41d6f6e13075df
Sergio G. Girona <sergioglez_8@hotmail.com>
2024-02-07 18:34:36 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+function App() {
+  return (
+    <>
+      <img src="./favicon.png" alt="Logo of coockbook" />
+      <h1>Cookbook</h1>
+      <p>The social network for recipe pirates</p>
+    </>
+  );
+}
+
+export default App;
